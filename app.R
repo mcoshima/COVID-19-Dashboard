950527714ba032d6c0ea78c649a5670612e6aa2d
Meg <megumi.oshima@usm.edu>
2020-07-12 20:08:26 -0500

    added shiny app

---
+++ app.R
@@ -0,0 +1,234 @@
+#
+# This is a Shiny dashboard using the COVID19 package.
+#
+
+#library(devtools)
+#devtools::install_github("nik01010/dashboardthemes")
+library(tidyverse)
+library(shiny)
+library(shinydashboard)
+library(COVID19)
+library(dashboardthemes)
+
+
+us <- covid19(country = "US", level = 2, raw = TRUE)
+territories <- c("Guam", "American Samoa", "Northern Mariana Islands", "Virgin Islands", "Puerto Rico")
+
+ST <- us %>% 
+    ungroup() %>%
+    filter(!grepl(paste(territories, collapse="|"), administrative_area_level_2)) %>% 
+    distinct(administrative_area_level_2) %>% 
+    arrange(administrative_area_level_2) %>% 
+    pull()
+
+counties <- covid19(country = "US", level = 3, raw = TRUE)
+
+
+source("theme_diy.R")
+
+
+ui <- dashboardPage(
+    dashboardHeader(
+        title = logo_nord
+    ),
+    dashboardSidebar(disable = TRUE),
+                     
+    dashboardBody(
+        
+            theme_nord, 
+        fluidRow(
+            box(
+                selectInput("state", h2("Select a State"), ST)
+            ),
+            box(
+                width = 5,
+                align = "center",
+                title = "Total Confirmed Cases in the US",
+                h1(textOutput("total_cases")),
+                solidHeader = TRUE,
+                status = "warning"
+            )
+        ),
+        fluidRow(
+                    box(width = 6,
+                        title = "Confirmed Cases",
+                        plotOutput("cases_chloro")
+                        ),
+                    box(width = 6,
+                        title = "Total Deaths in Each County",
+                        plotOutput("deaths_cholor")
+                        )
+                ),
+        
+                fluidRow(
+                    box(width = 4,
+                        align = "center",
+                        title = "Number of Tests in the State",
+                        h1(textOutput("N_tests")), 
+                        status = "success",
+                        solidHeader = TRUE
+                    ),
+                    box(
+                        width = 4,
+                        align = "center",
+                        title = "Number of Cases in the State",
+                        h1(textOutput("N_confirmed")), 
+                        status = "info",
+                        solidHeader = TRUE
+                    ),
+                    box(width = 4,
+                        align = "center",
+                        title = "Deaths",
+                        h1(textOutput("N_deaths")),
+                        status = "danger",
+                        solidHeader = TRUE
+                        
+                    )),
+                
+                fluidRow(
+                       box(
+                           width = 6,
+                           title = "Number of confirmed cases over time",
+                           plotOutput("all_cases")
+                       ),
+                       box(
+                           width = 5,
+                           title = "Number of confirmed cases over the past week",
+                           plotOutput("recent_cases")
+                       )
+                       
+                       )
+                )
+    )
+
+
+server <- shinyServer(function(input, output, session) {
+    
+    output$total_cases <- renderText({
+         us %>% 
+            ungroup() %>% 
+            summarise(sum(confirmed, na.rm = TRUE)) %>% 
+            pull() %>% 
+            prettyNum(big.mark = ",")
+            
+    })
+    
+    output$N_confirmed <- renderText({
+        us %>% 
+            ungroup() %>% 
+            group_by(administrative_area_level_2) %>% 
+            summarise(sum(confirmed, na.rm = TRUE)) %>% 
+            filter(str_detect(administrative_area_level_2, input$state)) %>% 
+            pull() %>% 
+            prettyNum(big.mark = ",")
+    })
+    
+    
+    output$N_deaths <- renderText({
+        us %>% 
+            ungroup() %>% 
+            group_by(administrative_area_level_2) %>% 
+            summarise(sum(deaths, na.rm = TRUE)) %>% 
+            filter(str_detect(administrative_area_level_2, input$state)) %>% 
+            pull() %>% 
+            prettyNum(big.mark = ",")
+    })
+    output$N_tests <- renderText({
+        us %>% 
+            ungroup() %>% 
+            group_by(administrative_area_level_2) %>% 
+            summarise(sum(tests, na.rm = TRUE)) %>% 
+            filter(str_detect(administrative_area_level_2, input$state)) %>% 
+            pull() %>% 
+            prettyNum(big.mark = ",")
+    })
+
+    output$cases_chloro <- renderPlot({
+        st <- counties %>% 
+            ungroup() %>% 
+            filter(str_detect(administrative_area_level_2, input$state)) %>% 
+            arrange(administrative_area_level_3) %>% 
+            rename(county_name = administrative_area_level_3) %>% 
+            select(county_name, date, tests, confirmed, deaths, recovered) %>% 
+            group_by(county_name) %>% 
+            summarise(Cases = sum(confirmed, na.rm = TRUE), 
+                      total_deaths = sum(deaths, na.rm = TRUE),
+                      total_tests = sum(tests, na.rm = TRUE),
+                      total_recovered = sum(recovered, na.rm = TRUE))
+        
+        urbnmapr::counties %>% 
+            filter(state_name ==  input$state) %>% 
+            mutate(county_name = str_remove_all(county_name, " County")) %>% 
+            left_join(st, by = "county_name") %>% 
+            ggplot(mapping = aes(long, lat, group = group, fill = Cases)) +
+            geom_polygon(color = "white") +
+            coord_map() +
+            scale_fill_gradient(low = "#ECEFF4", high = "#A3BE8C")+
+            theme_void()
+    })
+    
+    
+    output$deaths_cholor <- renderPlot({
+        st <- counties %>% 
+            ungroup() %>% 
+            filter(str_detect(administrative_area_level_2, input$state)) %>% 
+            arrange(administrative_area_level_3) %>% 
+            rename(county_name = administrative_area_level_3) %>% 
+            select(county_name, date, tests, confirmed, deaths, recovered) %>% 
+            group_by(county_name) %>% 
+            summarise(total_confirmed = sum(confirmed, na.rm = TRUE), 
+                      Cases = sum(deaths, na.rm = TRUE),
+                      total_tests = sum(tests, na.rm = TRUE),
+                      total_recovered = sum(recovered, na.rm = TRUE))
+        
+        urbnmapr::counties %>% 
+            filter(state_name ==  input$state) %>% 
+            mutate(county_name = str_remove_all(county_name, " County")) %>% 
+            left_join(st, by = "county_name") %>% 
+            ggplot(mapping = aes(long, lat, group = group, fill = Cases)) +
+            geom_polygon(color = "white") +
+            coord_map() +
+            scale_fill_gradient(low = "#ECEFF4", high = "#D08770")+
+            theme_void()
+        
+    })
+    
+    
+    output$all_cases <- renderPlot({
+        con.ave <- us %>% 
+            filter(str_detect(administrative_area_level_2, input$state)) %>% 
+            ungroup() %>% 
+            summarise(mean(confirmed)) %>% 
+            pull()
+        us %>% 
+            filter(str_detect(administrative_area_level_2, input$state)) %>% 
+            ungroup() %>% 
+            select(date, confirmed) %>% 
+            ggplot(aes(x = date, y = confirmed)) +
+            geom_col(fill = "#5E81AC") +
+            theme_classic() +
+            labs(x = "", y = "Number of Confirmed Cases")
+        
+    })
+    
+    output$recent_cases <- renderPlot({
+        
+        us %>% 
+            filter(str_detect(administrative_area_level_2, input$state)) %>% 
+            ungroup() %>% 
+            arrange(desc(date))  %>% 
+            slice(1:7) %>% 
+            select(date, confirmed) %>% 
+            ggplot(aes(x = date, y = confirmed)) +
+            geom_line(color = "#B48EAD", size = 1)  +
+            geom_point(color = "#5E81AC", size = 3.5) +
+            theme_classic() +
+            labs(x = "", y = "Number of Confirmed Cases")
+        
+        
+        
+    })
+    
+})
+
+shinyApp(ui, server)
---
+++ theme_diy.R
@@ -0,0 +1,114 @@
+theme_nord <- shinyDashboardThemeDIY(
+  
+  ### general
+  appFontFamily = "Arial"
+  ,appFontColor = "#D8DEE9"
+  ,primaryFontColor = "#434C5E"
+  ,infoFontColor = "#434C5E"
+  ,successFontColor = "#434C5E"
+  ,warningFontColor = "#434C5E"
+  ,dangerFontColor = "#434C5E"
+  ,bodyBackColor = "#2E3440"
+  
+  ### header
+  ,logoBackColor = "#3B4252"
+  
+  ,headerButtonBackColor = "#3B4252"
+  ,headerButtonIconColor = "#4C566A"
+  ,headerButtonBackColorHover = "#3B4252"
+  ,headerButtonIconColorHover = "#81A1C1"
+  
+  ,headerBackColor = "#3B4252"
+  ,headerBoxShadowColor = ""
+  ,headerBoxShadowSize = "0px 0px 0px"
+  
+  ### sidebar
+  ,sidebarBackColor = "#434C5E"
+  ,sidebarPadding = 0
+  
+  ,sidebarMenuBackColor = "transparent"
+  ,sidebarMenuPadding = 0
+  ,sidebarMenuBorderRadius = 0
+  
+  ,sidebarShadowRadius = ""
+  ,sidebarShadowColor = "0px 0px 0px"
+  
+  ,sidebarUserTextColor = "#D8DEE9"
+  
+  ,sidebarSearchBackColor = "#4C566A"
+  ,sidebarSearchIconColor = "#E5E9F0"
+  ,sidebarSearchBorderColor = "#4C566A"
+  
+  ,sidebarTabTextColor = "#ECEFF4"
+  ,sidebarTabTextSize = 14
+  ,sidebarTabBorderStyle = "none"
+  ,sidebarTabBorderColor = "none"
+  ,sidebarTabBorderWidth = 0
+  
+  ,sidebarTabBackColorSelected = "#3B4252"
+  ,sidebarTabTextColorSelected = "#ECEFF4"
+  ,sidebarTabRadiusSelected = "5px"
+  
+  ,sidebarTabBackColorHover = "#3B4252"
+  ,sidebarTabTextColorHover = "#81A1C1"
+  ,sidebarTabBorderStyleHover = "none"
+  ,sidebarTabBorderColorHover = "none"
+  ,sidebarTabBorderWidthHover = 0
+  ,sidebarTabRadiusHover = "5px"
+  
+  ### boxes
+  ,boxBackColor = "#434C5E"
+  ,boxBorderRadius = 5
+  ,boxShadowSize = "0px 0px 0px"
+  ,boxShadowColor = ""
+  ,boxTitleSize = 18
+  ,boxDefaultColor = "#434C5E"
+  ,boxPrimaryColor = "#88C0D0"
+  ,boxInfoColor = "#8FBCBB"
+  ,boxSuccessColor = "#A3BE8C"
+  ,boxWarningColor = "#EBCB8B"
+  ,boxDangerColor = "#BF616A"
+  
+  ,tabBoxTabColor = "#434C5E"
+  ,tabBoxTabTextSize = 16
+  ,tabBoxTabTextColor = "#D8DEE9"
+  ,tabBoxTabTextColorSelected = "#D8DEE9"
+  ,tabBoxBackColor = "#434C5E"
+  ,tabBoxHighlightColor = "#4C566A"
+  ,tabBoxBorderRadius = 5
+  
+  ### inputs
+  ,buttonBackColor = "#D8DEE9"
+  ,buttonTextColor = "#2E3440"
+  ,buttonBorderColor = "#2E3440"
+  ,buttonBorderRadius = 5
+  
+  ,buttonBackColorHover = "#E5E9F0"
+  ,buttonTextColorHover = "#3B4252"
+  ,buttonBorderColorHover = "#2E3440"
+  
+  ,textboxBackColor = "#4C566A"
+  ,textboxBorderColor = "#2E3440"
+  ,textboxBorderRadius = 5
+  ,textboxBackColorSelect = "#3B4252"
+  ,textboxBorderColorSelect = "#2E3440"
+  
+  ### tables
+  ,tableBackColor = "#4C566A"
+  ,tableBorderColor = "#2E3440"
+  ,tableBorderTopSize = 1
+  ,tableBorderRowSize = 1
+  
+)
+
+
+logo_nord <- shinyDashboardLogoDIY(
+  boldText = "COVID-19"
+  ,mainText = "in Your State"
+  ,textSize = 18
+  ,badgeText = ""
+  ,badgeTextColor = ""
+  ,badgeTextSize = 0
+  ,badgeBackColor = "#3B4252"
+  ,badgeBorderRadius = 0
+)
